d1caf3faede922a2ef358d81a5b4cd3804a47070
derek964 <zhaoboliang964@163.com>
2021-08-17 00:27:40 +0800

    创建前端页面，主要是导航及各个页面框架

--- stu/views.py
+++ stu/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def stuffinfo(request):
+    return render(request, 'stuffinfo.html')
+
+def dept_setting(request):
+    return render(request, 'dept_setting.html')
+
+def proj_setting(request):
+    return render(request, 'proj_setting.html')
+
+def stuff_search(request):
+    return render(request, 'stuff_search.html')
