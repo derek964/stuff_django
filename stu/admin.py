6028788d94eb0d34bd24517d0812c8e85aed9903
derek964 <zhaoboliang964@163.com>
2021-08-17 22:49:58 +0800

    修改admin登录后设置信息部分，可填写部门，填写项目，增加员工

--- stu/admin.py
+++ stu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from stu.models import *
 
 # Register your models here.
+
+class Newdept(admin.ModelAdmin):
+    list_display = ['deptcode','deptname']
+
+class Newproject(admin.ModelAdmin):
+    list_display = ['projcode', 'projname']
+
+class Newstuff(admin.ModelAdmin):
+    list_display = ['scode','sname','deptname','projname']
+
+admin.site.register(dept,Newdept)
+admin.site.register(project, Newproject)
+admin.site.register(stuff, Newstuff)
--- stu/models.py
+++ stu/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class dept(models.Model):
+    deptcode = models.CharField(max_length=10, unique=True, primary_key=True)
+    deptname = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.deptname
+
+class project(models.Model):
+    projcode = models.CharField(max_length=10, unique=True, primary_key=True)
+    projname = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.projname
+
+class stuff(models.Model):
+    scode = models.CharField(max_length=10, unique=True, primary_key=True)
+    sname = models.CharField(max_length=10, unique=True)
+    deptname = models.ForeignKey(dept, on_delete=models.CASCADE)
+    projname = models.ForeignKey(project, on_delete=models.CASCADE)
