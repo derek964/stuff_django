d448f294eb79e91bac59ff8519fe15ac136b4919
derek964 <zhaoboliang964@163.com>
2021-08-17 23:59:49 +0800

    修改stuinfo展示后台数据，前端页面使用表格循环语句

--- stu/views.py
+++ stu/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from stu.models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def stuffinfo(request):
-    return render(request, 'stuffinfo.html')
+    stuffinfos = stuff.objects.all()
+    return render(request, 'stuffinfo.html', context=locals())
 
 def dept_setting(request):
     return render(request, 'dept_setting.html')
